c4aeea421f5997668173075416ea57e71d714764
ruthijimenez <69929099+ruthijimenez@users.noreply.github.com>
2024-03-24 15:28:44 -0700

    functionality with tailwind

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
 mongoose.connect('mongodb://host.docker.internal:27017/service-catalog')
+//mongoose.connect('mongodb://localhost:27017/service-catalog')
      .then(() => console.log('Connected to MongoDB'))
      .catch(err => console.error('Could not connect to MongoDB', err));
 
